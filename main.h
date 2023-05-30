cf9574b6349a8b19076d20b695d326bd146e1044
tryingdaily <zanelemankazana@gmail.com>
2023-05-30 13:31:51 -0700

    Added Task 0

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list c);
+int print_string(va_list s);
+int print_int(va_list i);
+int print_dec(va_list d);
+/**
+  * struct code_format - Struct format
+  *
+  * @sc: specifiers
+  * @f: function associated
+  */
+typedef struct code_format
+{
+	char *sc;
+	int (*f)(va_list);
+} code_f;
+
+#endif /* MAIN_H */
+
