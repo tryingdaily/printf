6f0acab7964f2f911c6ca7dbcf9e90f46a3299af
Master Bee <lullaluve@gmail.com>
2023-05-30 13:47:53 -0700

    Redoing Task 0

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 void print_buffer(char buffer[], int *buff_ind);
 /**
-  * _printf – Printf function
+  * _printf - Printf function
   * @format: format.
   * Return: Printed chars.
   */
